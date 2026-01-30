ac5e068481ebdf5999e591a99dbc8a89924b5955
abdelkarimnakkahi <nakkahi.abdelkarim@gmail.com>
2026-01-30 20:39:02 +0100

    fix the testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,9 +5,9 @@ function Testimonials() {
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">
         What people are saying
       </h2>
-      <div className="flex flex-wrap justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {testimonials.map((testimonial, index) => (
-          <div key={index} className="w-full sm:w-1/2 lg:w-1/3 py-2">
+          <div key={index}>
             <div className="bg-neutral rounded-md p-6 text-md border border-neutral-800 font-thin">
               <p>{testimonial.text}</p>
               <div className="flex mt-8 items-center">
